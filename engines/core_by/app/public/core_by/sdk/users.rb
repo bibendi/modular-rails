d28a61d47563d99fb1ec5e3d1e23518d1d6a8812
Misha Merkushin <merkushin.m.s@gmail.com>
2021-11-22 16:30:26 +0300

    feat: use .to_entity

--- engines/core_by/app/public/core_by/sdk/users.rb
+++ engines/core_by/app/public/core_by/sdk/users.rb
@@ -7,23 +7,22 @@ module CoreBy
 
       def create(email: nil)
         if (form = CoreBy::Users::CreateForm.new(email: email)).save
-          # May be better to implement a magic method? `ActiveRecord::Base.to_entity`
-          Resonad.success Entities::User.new(form.user)
+          Resonad.success form.user&.to_entity
         else
           Resonad.failure form.errors
         end
       end
 
       def find_by_id(id)
-        Entities::User.from(User.kept.find_by(id: id))
+        User.kept.find_by(id: id)&.to_entity
       end
 
       def find_by_external_id(external_id)
-        Entities::User.from(User.kept.find_by(external_id: external_id))
+        User.kept.find_by(external_id: external_id)&.to_entity
       end
 
       def find_by_email(email)
-        Entities::User.from(User.kept.find_by_email(email))
+        User.kept.find_by_email(email)&.to_entity
       end
 
       def discard!(user_id)
